2aff25787e9ba74d0c359a0aae6c27607e89605d
Piotr Misiak <Piotr_misiak6@wp.pl>
2021-06-11 23:44:33 +0200

    Adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -7,13 +7,13 @@
         {
             content: "Zadanie2",
             done: true,
-        }, 
+        },
     ];
 
     const render = () => {
         let htmlString = "";
 
-        for(const task of tasks){
+        for (const task of tasks) {
             htmlString += `
             <li>
             ${task.content}
@@ -22,10 +22,32 @@
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
     };
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+        
+        addNewTask(newTaskContent);
+    };
+
 
     const init = () => {
         render();
 
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
