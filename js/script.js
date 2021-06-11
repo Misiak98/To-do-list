841f0040941f0213ec8ff5798620e495e1b69796
Piotr Misiak <Piotr_misiak6@wp.pl>
2021-06-11 23:56:34 +0200

    remove Task

--- js/script.js
+++ js/script.js
@@ -10,26 +10,46 @@
         },
     ];
 
-    const render = () => {
-        let htmlString = "";
 
-        for (const task of tasks) {
-            htmlString += `
-            <li>
-            ${task.content}
-            </li>
-            `;
-        }
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
     const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
         render();
     };
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
 
-const onFormSubmit = (event) => {
+
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li>
+            <button class = "js-remove">usuń</button>
+            ${task.content}
+            </li>
+            `;
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButtons, index) => {
+            removeButtons.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+    };
+
+
+
+
+    const onFormSubmit = (event) => {
         event.preventDefault();
 
         const newTaskContent = document.querySelector(".js-newTask").value.trim();
@@ -37,7 +57,7 @@ const onFormSubmit = (event) => {
         if (newTaskContent === "") {
             return;
         }
-        
+
         addNewTask(newTaskContent);
     };
 
